57aa454d7cff7ff0e19c3e776526ccc7055498a1
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-02-14 18:45:49 +0500

    14-16

---
+++ java/app.js
@@ -0,0 +1,41 @@
+Q:1
+// let arry = [];
+
+Q:2
+// let students = [];
+
+Q:3
+
+// let a = ["x", "y", "z"];
+
+// console.log(a);
+
+Q:4
+
+// let b = [1, 2, 3, 4];
+
+// console.log(b);
+
+
+Q:5
+
+// let boolean = [true, false]
+
+// console.log(boolean);
+
+Q:6
+
+// let mix = [1, 2, "x", "y", true, false]
+
+// console.log(mix);
+
+Q:7
+
+document.write ("<h1>Qualifications:</h1>")
+
+let edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.", "Phil.", "PhD"]
+
+document.write(edu [0]);
+document.write(edu [1]);
+document.write(edu [2]);
+document.write(edu [3]);
